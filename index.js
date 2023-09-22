b2507785b122882a41b7d727652927a45211b8a6
kushal <kushalbhetuwal@gmail.com>
2023-09-22 11:23:56 +1000

    first

--- index.js
+++ index.js
@@ -3,17 +3,23 @@ let index =0;
 function isLetter(letter) {
     return /^[a-zA-Z]$/.test(letter);
 }
+function focusnext() {
+  if (index < buttons.length) {
+    buttons[index].focus();
+    console.log(buttons[index]);
+    index++;
+  }
+}
+
 
 buttons.forEach((button) => {
-    button.addEventListener('keydown', (event) => {
+    button.addEventListener("keydown", function (event) {
         if (!isLetter(event.key)) {
             event.preventDefault();
         } else {
-            if (button.innerText.trim() === "") {
                 button.innerText = event.key;
-                button[index].focus()
-                index++;
-            }
+                focusnext();
+            
         }
     });
 });
