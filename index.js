b930515416a851c4f53e3cadb2ed0d51e2148bea
kushal <kushalbhetuwal@gmail.com>
2023-09-24 11:47:53 +1000

    prettier

--- index.js
+++ index.js
@@ -1,25 +1,47 @@
-const buttons = document.querySelectorAll('button');
-let index =0;
+const buttons = document.querySelectorAll(".button");
+const button1 = buttons[0].children;
+
+let index = 1;
+buttonpressed = false;
+let word = "CRANE";
+let words = "";
+
 function isLetter(letter) {
-    return /^[a-zA-Z]$/.test(letter);
+  return /^[a-zA-Z]$/.test(letter);
 }
+
 function focusnext() {
-  if (index < buttons.length) {
-    buttons[index].focus();
-    console.log(buttons[index]);
-    index++;
+  button1[index].focus();
+  buttonpressed = false;
+}
+count = 0;
+function takeword(word1) {
+  if (count > 3) {
+    if (word1 === word) {
+      alert("you are correct");
+    } else {
+      alert("you have entered the incorrect word:")
+      
+    }
   }
+  count++;
+
+  return;
 }
 
-
-buttons.forEach((button) => {
-    button.addEventListener("keydown", function (event) {
-        if (!isLetter(event.key)) {
-            event.preventDefault();
-        } else {
-                button.innerText = event.key;
-                focusnext();
-            
-        }
-    });
+Array.from(button1).forEach((button) => {
+  button.addEventListener("keydown", (event) => {
+    if (!isLetter(event.key)) {
+      event.preventDefault();
+    } else {
+      if (!buttonpressed) {
+        button.innerText = event.key.toUpperCase();
+        words = words + button.innerText;
+        buttonpressed = true;
+        focusnext();
+        index++;
+      }
+    }
+    takeword(words);
+  });
 });
